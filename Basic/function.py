ec4c7af47520889fdba5a3b734ee085f2bfca3d9
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-05-16 12:19:56 +0600

    function

---
+++ Basic/function.py
@@ -0,0 +1,16 @@
+def Name():
+    print("Inside the function")
+
+
+print("Outside of the function")
+Name()
+
+# add
+
+
+def add(x, y):
+    return x+y
+
+
+result = add(10, 5)
+print("Result: ", result)
