936e62368d1f6f396d4a44b0a5c4227301bdc012
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-05-16 11:09:35 +0600

    loop

---
+++ Basic/loop.py
@@ -0,0 +1,14 @@
+a = ['CSE', 'EEE', 'CIVIL', 'LAW']
+for dept in a:
+    print(dept)
+
+p = [10, 20, 30, 40, 50]
+total = 0
+for element in p:
+    total += element
+    print(total)
+
+all = 0
+for i in range(1, 30):
+    all += i
+    print(all)
---
+++ Basic/math.py
@@ -0,0 +1,21 @@
+import math
+
+x = 10
+
+print(math.pow(x, 4))
+
+print(math.sqrt(x))
+
+print(math.pi)
+
+print(math.e)
+
+print(math.factorial(x))
+
+print(math.remainder(x, 2))
+
+print(math.tau)
+
+print(math.inf)
+
+print(math.nan)
